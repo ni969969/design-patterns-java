a6d4744f711f6506c6763e1beeae48bd43e997c9
Dani <145723813+ni969969@users.noreply.github.com>
2026-07-04 18:49:47 -0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Projeto de Design Patterns em Java");
+        System.out.println("Padrões utilizados:");
+        System.out.println("- Singleton");
+        System.out.println("- Factory");
+        System.out.println("- Strategy");
+    }
+}
